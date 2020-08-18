0f3954f720cb59e044aee77f3bcce75db6b87735
Stephan Deist <sd@fino.digital>
2020-08-18 11:57:42 +0200

    fix

--- src/device/commands/command.handler.ts
+++ src/device/commands/command.handler.ts
@@ -1,14 +1,21 @@
-import { Injectable } from '@nestjs/common';
-import { RpcException } from '@nestjs/microservices';
+import { Injectable, Inject } from '@nestjs/common';
+import { RpcException, ClientKafka } from '@nestjs/microservices';
 
 import { DeviceService } from '../device.service';
 import { Device } from '../device.schema';
 import { Command } from './command';
 import { CreateDeviceCommand } from './createDevice.command';
+import { of } from 'rxjs';
+import { Config } from 'src/config/config.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class CommandHandler {
-  constructor(private deviceService: DeviceService) {}
+  constructor(
+    private deviceService: DeviceService,
+    @Inject('KAFKA_SERVICE') private kafkaClient: ClientKafka,
+    @Inject('CONFIG') private config: Config,
+  ) {}
 
   async handler(command: Command): Promise<Device> {
     switch (command.action) {
@@ -20,6 +27,17 @@ export class CommandHandler {
   }
 
   private async handleCreateDeviceCommand(command: CreateDeviceCommand) {
-    return this.deviceService.createOne(command.data);
+    const device = await this.deviceService.createOne(command.data);
+    const event = {
+      id: uuid(),
+      type: 'event',
+      action: 'FraunhoferDeviceUpdated',
+      timestamp: Date.now(),
+      data: device,
+    };
+
+    this.kafkaClient.emit(`${this.config.kafka.prefix}-device-event`, event);
+
+    return device;
   }
 }
